a04bc4fdc44b3d98f4a77170c045b51c69260160
Ashwini Mathpati <110845470+MathpatiAshwini@users.noreply.github.com>
2022-12-21 10:13:44 +0530

    Add files via upload

---
+++ zip.py
@@ -0,0 +1,5 @@
+z={'ashwini':88,'aarti':86,'vishakha':79,'ankita':70}
+p={'ashwini':87,'bhagyshri':75,'vaishnvi':85,'vijaya':86,'manisha':81}
+zipped=(zip(z,p))
+for (a,b )in zipped:
+    print(a,b)
